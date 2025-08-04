42b0be5c6faf7d1e2561a118a5f26a40a1b88591
ohfabe <fabiancorluka@gmail.com>
2025-08-04 19:54:31 -0400

    fetch requests done

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -4,12 +4,30 @@ const Login = () => {
     const [username, setUsername] = useState('')
      const [password, setPassword] = useState('')
 
+        const handlerSubmit = async() => {
+        e.preventDefault();
+        try {
+            const response = await fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, password})
+            })
+            const data = response.json()
+            console.log(data)
+            setUsername('')
+            setPassword('')
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div id = 'login'>
             <h1 id = 'login-title'>Login</h1>
-            <form id = 'login-form'>
-                <input id = 'login-username' type = 'text' placeholder = 'Username...' />
-                  <input id = 'login-password' type = 'password' placeholder = 'Password...' />
+            <form id = 'login-form' onSubmit = {handlerSubmit}>
+                <input id = 'login-username' type = 'text' placeholder = 'Username...' onChange = {(e) => setUsername(e.target.value)}/>
+                  <input id = 'login-password' type = 'password' placeholder = 'Password...' onChange = {(e) => setPassword(e.target.value)}/>
                   <button type = 'submit' id = 'login-submit'>Submit</button>
             </form>
         </div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './Navbar.css'
-import './App.css'
+import '/css/Navbar.css'
+import '/css/App.css'
+import '/css/Home.css'
+import '/css/Signup.css'
+import '/css/Login.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
