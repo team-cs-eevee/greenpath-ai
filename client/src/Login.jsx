7dd425b21ecfd932f4234adbf0c14148451e6e15
emilymcge <emilymcgettrick@gmail.com>
2025-08-05 14:50:01 -0400

    fix(login/signup): e in event handler, fields clear after submit

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,37 +1,57 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 const Login = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-        const handlerSubmit = async() => {
-        e.preventDefault();
-        try {
-            const response = await fetch('/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({username, password})
-            })
-            const data = await response.json()
-            console.log(data)
-            setUsername('')
-            setPassword('')
-        } catch (error) {
-            console.log(error)
-        }
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await response.json();
+      console.log(data);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.log(error);
     }
-    return (
-        <div id = 'login'>
-            <h1 id = 'login-title'>Login</h1>
-            <form id = 'login-form' onSubmit = {handlerSubmit}>
-                <input id = 'login-username' type = 'text' placeholder = 'Username...' onChange = {(e) => setUsername(e.target.value)}/>
-                <input id = 'login-password' type = 'password' placeholder = 'Password...' onChange = {(e) => setPassword(e.target.value)}/>
-                <button type = 'submit' id = 'login-submit'>Submit</button>
-            </form>
-        </div>
-    )
-}
+  };
+  return (
+    <div id='login'>
+      <h1 id='login-title'>Login</h1>
+      <form
+        id='login-form'
+        onSubmit={handlerSubmit}
+      >
+        <input
+          id='login-username'
+          type='text'
+          placeholder='Username...'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          id='login-password'
+          type='password'
+          placeholder='Password...'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type='submit'
+          id='login-submit'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,36 +1,56 @@
-import { useState } from "react"
+import { useState } from 'react';
 const Signup = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-       const handlerSubmit = async() => {
-        e.preventDefault();
-        try {
-            const response = await fetch('/api/signup', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({username, password})
-            })
-            const data = await response.json()
-            console.log(data)
-            setUsername('')
-            setPassword('')
-        } catch (error) {
-            console.log(error)
-        }
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await response.json();
+      console.log(data);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.log(error);
     }
-    return (
-        <div id = 'signup'>
-            <h1 id = 'signup-title'>Sign Up</h1>
-            <form id = 'signup-form' onSubmit = {handlerSubmit}>
-                <input id = 'signup-username' type = 'text' placeholder = 'Username...' onChange={(e) => setUsername(e.target.value)}/>
-                <input id = 'signup-password' type = 'password' placeholder = 'Password...' onChange={(e) => setPassword(e.target.value)}/>
-                <button type = 'submit' id = 'signup-submit'>Submit</button>
-            </form>
-        </div>
-    )
-}
+  };
+  return (
+    <div id='signup'>
+      <h1 id='signup-title'>Sign Up</h1>
+      <form
+        id='signup-form'
+        onSubmit={handlerSubmit}
+      >
+        <input
+          id='signup-username'
+          type='text'
+          placeholder='Username...'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          id='signup-password'
+          type='password'
+          placeholder='Password...'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type='submit'
+          id='signup-submit'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
